a391ac9cc303691458ae3df2b79842f0d49143b7
Natawon <natawon9@gmail.com>
2023-03-24 20:28:26 +0000

    add file

--- php-app.php
+++ php-app.php
@@ -7,14 +7,19 @@
 	$commands = array(
 		'echo $PWD',
 		'whoami',
-		'git --version',
-        'git push origin master'   
+		'git reset --hard HEAD',
+		'git pull',
+		'git status',
+		'git submodule sync',
+		'git submodule update',
+		'git submodule status',
+          
 	);
 	// Run the commands for output
 	$output = '';
 	foreach($commands AS $command){
 		// Run it
-		$tmp = shell_exec($command);
+		$tmp = exec($command);
 		// Output
 		$output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
 		$output .= htmlentities(trim($tmp)) . "\n";
